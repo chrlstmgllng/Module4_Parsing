3af19db51362b49584e1b00ee680a5bf88e0f415
NetAcad DEVASC <changeme@example.com>
2024-10-21 14:56:22 +0000

    Initial commit with existing files

---
+++ sample_jsonparse.py
@@ -0,0 +1,12 @@
+import json
+
+course = []
+
+with open('sample.json', 'r') as json_file:
+    x = json.load(json_file)
+
+    for courses in x ['certifications']:
+        print(courses['courses'])
+
+    for names in x ['certifications']:
+        print("Names:", names['courses'])
